c6eb129ac18269f5e3286b626e62cc87a14bb116
Douwe Raat <36298536+DouweRaat@users.noreply.github.com>
2021-02-18 08:41:13 +0100

    Add files via upload

---
+++ C-voor-unity/3. Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Opdracht_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double getal1;
+            double getal2;
+            double uitkomst;
+            string operatie;
+            Console.WriteLine("Eerste getal:");
+            getal1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tweede getal:");
+            getal2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Operatie (+, -, x of /):");
+            operatie = Console.ReadLine();
+            if (operatie == "+")
+            {
+                uitkomst = getal1 + getal2;
+                Console.WriteLine(getal1 + "+" + getal2 + "=" + uitkomst);
+            }
+            else if (operatie == "-")
+            {
+                uitkomst = getal1 - getal2;
+                Console.WriteLine(getal1 + "-" + getal2 + "=" + uitkomst);
+            }
+            else if (operatie == "x")
+            {
+                uitkomst = getal1 * getal2;
+                Console.WriteLine(getal1 + "x" + getal2 + "=" + uitkomst);
+            }
+            else if (operatie == "/")
+            {
+                if (getal2 == 0)
+                {
+                    Console.WriteLine("Je kan niet door 0 delen.");
+                }
+                else
+                {
+                    uitkomst = getal1 / getal2;
+                    Console.WriteLine(getal1 + "/" + getal2 + "=" + uitkomst);
+                }
+            }
+        }
+    }
+}
---
+++ C-voor-unity/4. Guess game/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "informatica";
+            string guessedWord = "";
+            for(int kansen = 3; kansen > 0 && guessedWord != secretWord; kansen --)
+            {
+                Console.Write("Enter guess: ");
+                guessedWord = Console.ReadLine();
+            }
+            if (guessedWord != secretWord)
+            {
+                Console.WriteLine("You've lost!");
+            }
+            else
+            {
+                Console.WriteLine("You've won!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
