73792f2c3423443cf40fd2ed7cdaab56513ebe05
Douwe Raat <36298536+DouweRaat@users.noreply.github.com>
2021-02-18 08:42:34 +0100

    Add files via upload

---
+++ C-voor-unity/5. Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nummers = new int[20];
+            int huidigeGetal = 1;
+            int vorigeGetal = 0;
+            int nieuweGetal = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                nummers[i] = huidigeGetal;
+                Console.WriteLine(nummers[i]);
+                nieuweGetal = huidigeGetal + vorigeGetal;
+                vorigeGetal = huidigeGetal;
+                huidigeGetal = nieuweGetal;
+            }
+            Console.ReadLine();
+        }
+    }
+}
