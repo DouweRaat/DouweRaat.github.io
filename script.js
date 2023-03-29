38caeeb1a25595a00cc7472116cd433584dfe7ba
Douwe Raat <36298536+DouweRaat@users.noreply.github.com>
2023-03-29 09:32:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,114 +1,34 @@
 //<------------------------------------------------ Image switcher ------------------------------------------------------------>
 
-legoImage = document.getElementById('LEGOimage');
-legoImages = ['images/LEGO1.png', 'images/LEGO2.jpg', 'images/LEGO3.png', 'images/LEGO4.jpg'];
-var legoIndex = 1;
-
-holoImage = document.getElementById('HOLOimage');
-holoImages = ['images/HOLO1.jpg', 'images/HOLO2.png', 'images/HOLO3.jpg','images/HOLO4.jpg', 'images/HOLO5.png'];
-var holoIndex = 1;
-
-questImage = document.getElementById('QUESTimage');
-questImages = ['images/QUEST1.png', 'images/QUEST2.png'];
-var questIndex = 1;
-
-bobImage = document.getElementById('BOBimage');
-bobImages = ['images/BOB1.PNG', 'images/BOB2.PNG', 'images/BOB3.PNG', 'images/BOB4.PNG', 'images/BOB5.PNG', 'images/BOB6.PNG', 'images/BOB7.PNG', 'images/BOB8.PNG', 'images/BOB9.PNG'];
-var bobIndex = 1;
-
-cppImage = document.getElementById('CPPimage');
-cppImages = ['images/C++1.svg.png', 'images/C++2.jpg'];
-var cppIndex = 1;
+var elements = [document.getElementById('LEGOimage'), document.getElementById('HOLOimage'), questImage = document.getElementById('QUESTimage'), bobImage = document.getElementById('BOBimage'), cppImage = document.getElementById('CPPimage')];
+var images = [['images/LEGO1.png', 'images/LEGO2.jpg', 'images/LEGO3.png', 'images/LEGO4.jpg'], ['images/HOLO1.jpg', 'images/HOLO2.png', 'images/HOLO3.jpg','images/HOLO4.jpg', 'images/HOLO5.png'], ['images/QUEST1.png', 'images/QUEST2.png'], ['images/BOB1.PNG', 'images/BOB2.PNG', 'images/BOB3.PNG', 'images/BOB4.PNG', 'images/BOB5.PNG', 'images/BOB6.PNG', 'images/BOB7.PNG', 'images/BOB8.PNG', 'images/BOB9.PNG'], ['images/C++1.svg.png', 'images/C++2.jpg']];
+var indexes = [1, 1, 1, 1, 1];
 
 function updateImages() {
-    legoImage.src = legoImages[legoIndex];
-    legoIndex = (legoIndex + 1) % legoImages.length;
-    holoImage.src = holoImages[holoIndex];
-    holoIndex = (holoIndex + 1) % holoImages.length;
-    questImage.src = questImages[questIndex];
-    questIndex = (questIndex + 1) % questImages.length;
-    bobImage.src = bobImages[bobIndex];
-    bobIndex = (bobIndex + 1) % bobImages.length;
-    cppImage.src = cppImages[cppIndex];
-    cppIndex = (cppIndex + 1) % cppImages.length;
+    for (let i = 0; i < indexes.length; i++) {
+        elements[i].src = images[i][indexes[i]];
+        indexes[i] = (indexes[i] + 1) % images[i].length;
+    }
 }
 setInterval(updateImages, 5000)
 
 //<------------------------------------------------ Pop-up code ------------------------------------------------------------>
-//Lego
-var legoModal = document.getElementById("legoModal");
-var legoSpan = document.getElementsByClassName("close")[0];
 
-legoImage.onclick = function() {
-    legoModal.style.display = "block";
-}
-
-legoSpan.onclick = function() {
-    legoModal.style.display = "none";
-}
-
-//holo
-var holoModal = document.getElementById("holoModal");
-var holoSpan = document.getElementsByClassName("close")[1];
-
-holoImage.onclick = function() {
-    holoModal.style.display = "block";
-}
-
-holoSpan.onclick = function() {
-    holoModal.style.display = "none";
-}
-
-//quest
-var questModal = document.getElementById("questModal");
-var questSpan = document.getElementsByClassName("close")[2];
-
-questImage.onclick = function() {
-    questModal.style.display = "block";
-}
-
-questSpan.onclick = function() {
-    questModal.style.display = "none";
-}
-
-//bob
-var bobModal = document.getElementById("bobModal");
-var bobSpan = document.getElementsByClassName("close")[3];
-
-bobImage.onclick = function() {
-    bobModal.style.display = "block";
-}
-
-bobSpan.onclick = function() {
-    bobModal.style.display = "none";
-}
-
-//cpp
-var cppModal = document.getElementById("cppModal");
-var cppSpan = document.getElementsByClassName("close")[4];
-
-cppImage.onclick = function() {
-    cppModal.style.display = "block";
-}
-
-cppSpan.onclick = function() {
-    cppModal.style.display = "none";
+var modals = [document.getElementById("legoModal"), document.getElementById("holoModal"), document.getElementById("questModal"), document.getElementById("bobModal"), document.getElementById("cppModal")];
+var spans = [document.getElementsByClassName("close")[0], document.getElementsByClassName("close")[1], document.getElementsByClassName("close")[2], document.getElementsByClassName("close")[3], document.getElementsByClassName("close")[4]]
+for (let i = 0; i < elements.length; i++) {
+    elements[i].onclick = function() {
+        modals[i].style.display = "block";
+    }
+    spans[i].onclick = function() {
+        modals[i].style.display = "none";
+    }
 }
 
 window.onclick = function(event) {
-    if (event.target == legoModal) {
-        legoModal.style.display = "none";
-    }
-    else if (event.target == holoModal) {
-        holoModal.style.display = "none";
-    }
-    else if (event.target == questModal) {
-        questModal.style.display = "none";
-    }
-    else if (event.target == bobModal) {
-        bobModal.style.display = "none";
-    }
-    else if (event.target == cppModal) {
-        cppModal.style.display = "none";
+    for (let i = 0; i < elements.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
     }
 }
